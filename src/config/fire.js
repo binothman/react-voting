48246029bffcddb3c917f6876c49542552551f6b
Mohamed Binothman <binothman@clickapps.co>
2019-02-20 18:07:19 +0300

    init firebase

---
+++ src/config/fire.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyA4qbtqWUgUtuFb46IWW1KPwVEvJkqiMKE",
+  authDomain: "voting-91980.firebaseapp.com",
+  databaseURL: "https://voting-91980.firebaseio.com",
+  projectId: "voting-91980",
+  storageBucket: "voting-91980.appspot.com",
+  messagingSenderId: "723295294868"
+}
+
+const fire = firebase.initializeApp(config)
+
+export default fire
