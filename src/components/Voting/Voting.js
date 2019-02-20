219be1eb227d0f9a21a059cdcb70aaa37052b3e9
Mohamed Binothman <binothman@clickapps.co>
2019-02-20 18:04:00 +0300

    make design for voting page

--- src/components/Voting/Voting.js
+++ src/components/Voting/Voting.js
@@ -1,7 +1,51 @@
 import React from 'react'
+import { Card, Button, Container } from 'semantic-ui-react'
 
 const Voting = () => (
-  <div>Voting</div>
+  <Container style={{marginTop: 20}}>
+    <Card.Group itemsPerRow={4}>
+      <Card
+        image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+        header='Elliot Baker'
+        extra={<Button icon="thumbs up" primary fluid content="Vote" />}
+      />
+      <Card
+        image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+        header='Elliot Baker'
+        extra={<Button icon="thumbs up" primary fluid content="Vote" />}
+      />
+      <Card
+        image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+        header='Elliot Baker'
+        extra={<Button icon="thumbs up" primary fluid content="Vote" />}
+      />
+      <Card
+        image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+        header='Elliot Baker'
+        extra={<Button icon="thumbs up" primary fluid content="Vote" />}
+      />
+      <Card
+        image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+        header='Elliot Baker'
+        extra={<Button icon="thumbs up" primary fluid content="Vote" />}
+      />
+      <Card
+        image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+        header='Elliot Baker'
+        extra={<Button icon="thumbs up" primary fluid content="Vote" />}
+      />
+      <Card
+        image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+        header='Elliot Baker'
+        extra={<Button icon="thumbs up" primary fluid content="Vote" />}
+      />
+      <Card
+        image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+        header='Elliot Baker'
+        extra={<Button icon="thumbs up" primary fluid content="Vote" />}
+      />
+    </Card.Group>
+  </Container>
 )
 
 export default Voting
