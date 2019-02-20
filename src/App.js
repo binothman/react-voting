0d10a8d88b6e1ffb8ba00f5ee7c07a82f7f6c3c2
Mohamed Binothman <binothman@clickapps.co>
2019-02-20 17:47:30 +0300

    make design for login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { HashRouter } from 'react-router-dom'
 import RootRouter from './routers/RootRouter'
 
+// CSS
+import './app.css'
+import 'semantic-ui-css/semantic.min.css'
+
 class App extends Component {
   render() {
     return (
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Container, Form, Button, Grid } from 'semantic-ui-react'
 
 const Login = () => (
-  <div>Login</div>
+  <Container style={{marginTop: 50}}>
+    <Grid centered columns={1}>
+      <Grid.Column computer={8} mobile={16}>
+        <Form>
+          <Form.Field>
+            <input placeholder='Email' />
+          </Form.Field>
+          <Form.Field>
+            <input placeholder='Password' />
+          </Form.Field>
+          <Button primary type='submit'>Login</Button>
+          <Button basic type='submit'>Signup</Button>
+        </Form>
+      </Grid.Column>
+    </Grid>
+  </Container>
 )
 
 export default Login
